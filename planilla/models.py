ddac451fc19a26937601c64c9df45f1a92bd9c67
Oscar Salinas Mora <oscar.salinas.mora@me.com>
2025-06-25 10:46:58 -0600

    Sistema SELENE Django v0.0001

--- planilla/models.py
+++ planilla/models.py
@@ -1,16 +1,47 @@
 from django.db import models
 
 class Planilla(models.Model):
-    no = models.BigIntegerField(primary_key=True)
+    no = models.BigAutoField(primary_key=True)
     nombre = models.TextField(blank=True, null=True)
     adscripcion = models.TextField(blank=True, null=True)
     departamento = models.TextField(blank=True, null=True)
-    clave = models.BigIntegerField(blank=True, null=True)
+    clave = models.TextField(blank=True, null=True)
     rfc = models.TextField(blank=True, null=True)
     curp = models.TextField(blank=True, null=True)
     puesto = models.TextField(blank=True, null=True)
+    sindicato = models.TextField(blank=True, null=True)  # ← AGREGA ESTA LÍNEA
     fecha_de_ingreso = models.DateTimeField(blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # primero guarda y que la BD asigne el no
+        if self.no is None:
+            raise ValueError("El campo 'no' no puede ser None")    
+
     class Meta:
-        managed = False
-        db_table = 'planilla'
+        db_table = 'planilla'  # Le indica a Django que use la tabla 'planilla'
+
+class Adscripcion(models.Model):
+    nombre = models.CharField(max_length=100)
+    clave_adscripcion = models.CharField(max_length=20, blank=True, null=True)
+    municipio = models.CharField(max_length=100, blank=True, null=True)
+    zona = models.CharField(max_length=10, blank=True, null=True)
+    direccion = models.CharField(max_length=200, blank=True, null=True)
+    activo = models.CharField(max_length=100, default='1')  # o el valor que prefieras
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'adscripcion'  # Esta apunta a tabla 'adscripcion'
+
+class Plazas(models.Model):
+    codigo = models.CharField(max_length=20)
+    denominacion = models.CharField(max_length=255)
+    zona_economica = models.CharField(max_length=10)
+    nivel_salarial = models.CharField(max_length=10)
+    plazas = models.IntegerField()
+    horas = models.IntegerField()
+    
+    class Meta:
+        db_table = 'plazas'  # Esta apunta a tabla 'plazas'
--- planilla/urls.py
+++ planilla/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('altas/nueva/', views.altas_nueva, name='altas_nueva'),
+    path('altas/lista/', views.altas_lista, name='altas_lista'),
+    path('plazas/', views.altas_lista, name='control_plazas'),
     path('', views.lista_planilla, name='planilla'),
+    path('anexo-ejecucion/', views.anexo_ejecucion, name='anexo_ejecucion'),
+    path('historial/', views.historial, name='historial'),
+    path('debug-planteles/', views.debug_planteles),
+    path('planilla/eliminar/<int:no>/', views.eliminar_registro, name='eliminar_registro'),
+    path('consulta/', views.consulta, name='consulta'),
+    path('estadisticas/', views.estadisticas, name='estadisticas'),
+    path('debug-planteles/', views.dplanteles, name='dplanteles'),
 ]
