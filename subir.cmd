ef7565289856b324b50dc1da3267295dc71b8800
Oscar Salinas Mora <oscar.salinas.mora@me.com>
2025-06-27 15:55:21 -0600

    Descripcion de los cambios realizados %mydate% %mytime%

--- subir.cmd
+++ subir.cmd
@@ -1,3 +1,8 @@
 git add .
-git commit -m "Descripción de los cambios realizados"
+git commit -m "Descripcion de los cambios realizados %%mydate%% %%mytime%%"
 git push
+
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
